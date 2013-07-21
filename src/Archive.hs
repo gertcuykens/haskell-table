6a473a8d75abd3730a6a3e918f28a7b7a6eb994b
Gert Cuykens <gert.cuykens@gmail.com>
2013-07-21 23:22:46 +0200

    clean

--- src/Archive.hs
+++ src/Archive.hs
@@ -2,19 +2,25 @@
 import Control.Exception     ( bracket )
 import Data.Acid             ( AcidState, createCheckpoint, closeAcidState )
 import Data.Acid.Advanced    ( query', update' )
-import Data.Acid.Local       ( createArchive, openLocalState )
+--import Data.Acid.Local       ( createArchive, openLocalState )
 import Data.Acid.Remote      ( openRemoteState, sharedSecretPerform )
 import Data.ByteString.Char8 ( pack )
-import Data.Map              ( empty )
 import Network               ( PortID(PortNumber) )
 import Table                 ( UserMap(..), User(..), InsertKey(..), LookupKey(..))
 
-main :: IO ()
-main = do
-    --acid <- openLocalState $ UserMap empty
-    acid <- openRemoteState (sharedSecretPerform $ pack "12345") "localhost" (PortNumber 8080) :: IO (AcidState UserMap)
-    --_ <- update' acid (InsertKey "123" (User "" "" "" ""))
-    --_ <- query' acid (LookupKey "123")
+openAcidState :: IO (AcidState UserMap)
+openAcidState = openRemoteState (sharedSecretPerform $ pack "12345") "localhost" (PortNumber 8080)
+
+runAcidState :: AcidState UserMap -> IO ()
+runAcidState acid = do
+    _ <- update' acid (InsertKey "1" (User "1" "" "" ""))
+    _ <- update' acid (InsertKey "2" (User "2" "" "" ""))
+    _ <- update' acid (InsertKey "3" (User "3" "" "" ""))
+    _ <- update' acid (InsertKey "4" (User "4" "" "" ""))
+    p <- query' acid (LookupKey "4")
+    print p
     createCheckpoint acid
-    closeAcidState acid
+
+main :: IO ()
+main = bracket openAcidState closeAcidState runAcidState
 
