3faf7dbc050358457cdde6a0e9228c4044dfa14e
Gert Cuykens <gert.cuykens@gmail.com>
2013-07-23 06:11:06 +0200

    table

--- src/Archive.hs
+++ src/Archive.hs
@@ -15,8 +15,8 @@ runAcidState acid = do
     _ <- update' acid (InsertKey 1 (User "" "" ["User"]))
     _ <- update' acid (InsertKey 2 (User "" "" ["User"]))
     _ <- update' acid (InsertKey 3 (User "" "" ["User"]))
-    _ <- update' acid (InsertKey 4 (User "" "" ["User"]))
-    p <- query' acid (LookupKey 4)
+    _ <- update' acid (InsertKey 116469479527388802962 (User "" "" ["User","Admin"]))
+    p <- query' acid (LookupKey 116469479527388802962)
     print p
     createCheckpoint acid
 
